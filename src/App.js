96568784e277af912b7ec3fe80d6e044467b8596
PeHaa <info@pehaa.com>
2021-05-25 08:24:04 +0200

    start day2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Todos from "./components/Todos"
 import ColorModeContainer from "./components/ColorModeContainer"
+import Login from "./components/Login"
+import Navigation from "./components/Navigation"
+import Home from "./components/Home"
 
 function App() {
   return (
-    <ColorModeContainer>
-      <div className="container my-4">
-        <h1 className="text-center">ToDos App</h1>
-        <Todos />
-      </div>
-    </ColorModeContainer>
+    <Router>
+      <ColorModeContainer>
+        <Navigation />
+        <div className="container my-4">
+          <h1 className="text-center">ToDos App</h1>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/todos">
+              <Todos />
+            </Route>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+          </Switch>
+        </div>
+      </ColorModeContainer>
+    </Router>
   )
 }
 
